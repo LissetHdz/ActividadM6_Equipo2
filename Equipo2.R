4e14d9f557b86fd9398d51c860800e583d14f7f1
LissetHdz <lissethernandez002@gmail.com>
2024-02-19 23:23:53 -0600

    'Paso_1_y_2

---
+++ Equipo2.R
@@ -0,0 +1,17 @@
+#Lisset Hernández A01284611
+
+#1. Instalar y llamar librerias
+install.packages("cluster")
+install.packages("ggplot2")
+install.packages("data.table")
+install.packages("factoextra")
+
+
+library(cluster)
+library(ggplot2)
+library(data.table)
+library(factoextra)
+
+#2. Cargar data
+data <- read.csv("C:\Users\alanh\Downloads\wine.csv")
+
