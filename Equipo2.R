4a4da61a42f69663116a5dc96d4fb030f7ba4a59
hectordlgt <a01177960@tec.mx>
2024-02-20 17:05:30 -0600

    Parte 5 y 6 de Hector

--- Equipo2.R
+++ Equipo2.R
@@ -1,6 +1,6 @@
 #Lisset Hernández A01284611
 
-#1. Instalar y llamar librerias
+#Parte 1. Instalar y llamar librerias
 install.packages("cluster")
 install.packages("ggplot2")
 install.packages("data.table")
@@ -12,11 +12,20 @@ library(ggplot2)
 library(data.table)
 library(factoextra)
 
-#2. Cargar data
+#Parte 2. Cargar data
 data <- read.csv("C:\Users\alanh\Downloads\wine.csv")
 
-#3. Escalar la base de datos
+#Parte 3. Escalar la base de datos
 data_escalada<- scale(data)
 
-#Definir número de grupos
+#Parte 4. Definir número de grupos
 grupos <- 3
+
+#Parte 5. Generar los segmentos
+segmentos <- kmeans(df, grupos)
+segmentos
+
+#Parte 6. Asignar el grupo al que pertenece cada observación
+asignacion <- cbind(cluster = segmentos$cluster, datos)
+head(asignacion)
+
